f30370b40563db1af32c56c3e1114df1355f6c80
이승구 <leesg107@naver.com>
2024-12-09 13:48:01 +0900

    feat: first commit

---
+++ src/main/java/com/innercicle/ic2ratelimiter/Ic2RateLimiterApplication.java
@@ -0,0 +1,13 @@
+package com.innercicle.ic2ratelimiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ic2RateLimiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ic2RateLimiterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/innercicle/ic2ratelimiter/Ic2RateLimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innercicle.ic2ratelimiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ic2RateLimiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
