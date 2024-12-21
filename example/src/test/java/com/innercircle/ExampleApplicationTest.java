82b9d43dbcd5d2bcd905e0fc0df7942f04c4e53a
cyeji <98408267+cyeji@users.noreply.github.com>
2024-12-21 10:47:26 +0900

    Delete ExampleApplicationTest.java

--- example/src/test/java/com/innercircle/ExampleApplicationTest.java
+++ example/src/test/java/com/innercircle/ExampleApplicationTest.java
@@ -1,13 +1,1 @@
-package com.innercircle;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ExampleApplicationTest {
-
     @Test
-    void contextLoads() {
-    }
-
-}
